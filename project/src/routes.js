2c1e7f1c477d7974c5baffa81b236340b7fa6478
Diego Bolina <diego.bolina@gmail.com>
2017-05-22 20:21:07 -0300

    Remove unused file

--- project/src/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from './components/home/Home.vue';
-import Course from './components/course/Course.vue';
-
-export const routes = [
-
-    { path: '', component: Home },
-    { path: '/course/:id',  name:'course', component: Course }
-
-];
