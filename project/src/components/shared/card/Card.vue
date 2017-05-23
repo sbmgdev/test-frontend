77e1b58b9abf28533fa992d0fedb0be90aa32739
Diego Bolina <diego.bolina@gmail.com>
2017-05-22 22:35:27 -0300

    Add layout for courses list

--- project/src/components/shared/card/Card.vue
+++ project/src/components/shared/card/Card.vue
@@ -1,15 +1,23 @@
 <template>
 	<div>
 		<router-link :to="{ name: 'course', params: { id: id }}">
-			<h1>{{ id }}</h1>
-			 <h2>{{ title }}</h2>
-			 <p>{{ description }}</p>
-			 <p>{{ category }}</p>
-			 <span>De {{ start }} a {{ finish }}</span>
-			 <span>{{ city }}</span>
+			<md-card class="card-ripple" md-with-hover>
+			  <div class="pd-17">
+			      <p>{{ category }}</p>
+			      <h2>{{ title }}</h2>
+			      <span>{{ city }}</span>
+		      </div>
+			  <md-card-actions class="pd-17">
+			  <span class="date">De {{ start | moment("d/M") }} a {{ finish | moment("d/M/YYYY") }}</span>
+			    <md-button class="md-icon-button">
+			      <md-icon>today</md-icon>
+			    </md-button>
+			  </md-card-actions>
+			</md-card>
 		</router-link>
 	</div>
 </template>
+
 <script>
 	export default {
 	   props: ['id', 'title', 'description', 'category', 'start', 'finish', 'city' ]
@@ -17,5 +25,47 @@
 </script>
 
 <style lang="scss" scoped>
-
+	.md-card{
+		margin-bottom:25px;
+		min-height:270px;
+	}
+	hr{
+		width:100%;
+		border-color:#ccc;
+	}
+	p{
+		color:#757575;
+		font-size:12px;
+		margin:0;
+	}
+	h2{
+		color:#242424;
+		font-size:16px;
+		font-weight:normal;
+	}
+	span{
+		color:#757575;
+		font-size:14px;
+		&.date{
+			position:absolute;
+			left:17px;
+		}
+	}
+	a:not(.md-button):not(.md-bottom-bar-item):hover{
+		text-decoration:none;
+	}
+	.pd-17{
+		padding:17px;
+	}
+	.md-layout{
+		> div{
+			width:100%;
+		}
+	}
+	.md-card-actions{
+		position: absolute;
+	    bottom: 0;
+	    border-top: 1px solid #ccc;
+	    width: 100%;
+	}
 </style>
