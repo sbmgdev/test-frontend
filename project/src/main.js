d88d53d726606f319a860088456c4eb81351055e
Diego Bolina <diego.bolina@gmail.com>
2017-05-22 22:32:49 -0300

    Add new dependencies
    vue-material for layout
    vue-moment for standarize date formats

--- project/src/main.js
+++ project/src/main.js
@@ -4,10 +4,14 @@ import VueResource from 'vue-resource'
 import router from './router'
 import store from './store/index'
 import { sync } from 'vuex-router-sync'
+import VueMaterial from 'vue-material'
+import VueMoment from 'vue-moment'
 
 sync(store, router)
 
 Vue.use(VueResource)
+Vue.use(VueMaterial)
+Vue.use(VueMoment)
 
 Vue.config.productionTip = false
 
