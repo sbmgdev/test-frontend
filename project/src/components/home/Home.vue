82be7cec3ecb6b8db6c5d3154e42d704dc7c4080
Diego Bolina <diego.bolina@gmail.com>
2017-05-22 22:35:47 -0300

    Add modifications for couses list

--- project/src/components/home/Home.vue
+++ project/src/components/home/Home.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <div v-for="course in allCourses">
-      <my-card :id="course.id"
-               :title="course.title" 
-               :description="course.description"
-               :category="course.category"
-               :start="course.start"
-               :finish="course.finish" />
+    <div class="container">
+      <md-layout md-gutter="16">
+          <md-layout v-for="course in allCourses" md-flex-xsmall="100" md-flex-medium="50" md-flex-large="33"  md-flex-xlarge="33" :key="course.id"  >
+          <my-card :id="course.id"
+                   :title="course.title" 
+                   :description="course.description"
+                   :category="course.category"
+                   :start="course.start"
+                   :finish="course.finish"
+                   :city="course.address.city" />
+          </md-layout>
+      </md-layout>
     </div>
+
   </div>
 </template>
 
@@ -18,7 +24,7 @@ import Card from '../shared/card/Card.vue'
 
 export default {
   components: {
-   'my-card' : Card
+   'my-card' : Card,
   },
   mounted () {
     this.getAllCourses()
