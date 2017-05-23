6353b335dd90419cb31c0ed5ce214a8f567f7e20
Diego Bolina <diego.bolina@gmail.com>
2017-05-22 22:34:52 -0300

    Add new topbar component

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <router-view></router-view>
+  	<my-topbar />
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
+import Topbar from './components/shared/topbar/Topbar.vue'
+
+export default {
+  components: {
+   'my-topbar' : Topbar
+  },
+}
 </script>
 
 <style lang="scss">
-
+.container{
+	max-width:1030px;
+	margin:0 auto;
+	padding:20px 20px;
+}
+.pagina-enter-active, .pagina-leave-active {
+  transition: opacity .3s;
+}
+.pagina-enter, .pagina-leave-active {
+  opacity: 0;
+}
 </style>
---
+++ project/src/components/shared/topbar/Topbar.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<md-toolbar>
+			<router-link :to="{ name: 'home'}">
+			  <h1 class="md-title" style="flex: 1">Cursos</h1>
+
+		  	
+			</router-link>
+		</md-toolbar>
+	</div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+	.md-theme-default.md-toolbar{
+		background-color:#2196f3;
+	}
+	h1{
+		color:#ffffff;
+		&:hover{
+			color:#ccc;
+		}
+	}
+	a:not(.md-button):not(.md-bottom-bar-item):hover{
+		text-decoration:none;
+	}
+</style>
